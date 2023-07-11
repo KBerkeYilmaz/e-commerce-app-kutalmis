accc2845bff918accf9cd5fb41e1eec8730336a5
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2023-07-11 04:55:11 +0300

    Initial commit

--- app/views/pages/get.php
+++ app/views/pages/get.php
@@ -1,19 +1,19 @@
 <?php 
-// You might need to adjust these paths based on your project structure
-use App\Models\Product;
-require_once '../app/bootstrap.php';
+// // You might need to adjust these paths based on your project structure
+// use App\Models\Product;
+// require_once '../app/bootstrap.php';
 
 
-$productModel = new Product();
+// $productModel = new Product();
 
-$products = $productModel->getProducts();
-$json = json_encode($products);
+// $products = $productModel->getProducts();
+// $json = json_encode($products);
 
-// Based on the result, send a success or error response
-if ($json) {
-    http_response_code(200);
-    echo $json;
-} else {
-    http_response_code(500);
-    echo json_encode(['error' => 'Failed to get products']);
-}
+// // Based on the result, send a success or error response
+// if ($json) {
+//     http_response_code(200);
+//     echo $json;
+// } else {
+//     http_response_code(500);
+//     echo json_encode(['error' => 'Failed to get products']);
+// }
